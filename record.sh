69f0eed85589061d57bf3d07df628c6e415a7bd4
Hui Xue <hui.xue@nih.gov>
2023-03-11 19:53:00 -0500

    add record.sh

---
+++ record.sh
@@ -0,0 +1,7 @@
+# single task
+
+python ./classifier.py --option finetune --use_gpu --batch_size 64 --epochs 20 --lr 1e-5
+
+# multi-task
+
+python .\multitask_classifier.py --option finetune --dep --use_gpu --without_para --without_sts --lr 1e-5 --batch_size 64 --epochs 20
